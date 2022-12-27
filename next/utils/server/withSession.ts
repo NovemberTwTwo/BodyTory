ed65f5d709bad360ac3128747555f15df27a64a2
DaHyunOhe <peelsob4d@gmail.com>
2022-12-28 05:31:45 +0900

    fix: changed cookie max-age

--- next/utils/server/withSession.ts
+++ next/utils/server/withSession.ts
@@ -11,7 +11,7 @@ const cookieOptions = {
   cookieName: "userSession",
   password: process.env.COOKIE_PASSWORD!,
   cookieOptions: {
-    maxAge: 60 * 60 * 8,
+    maxAge: 60 * 60 * 24,
   },
 };
 export function withApiSession(fn: any) {
