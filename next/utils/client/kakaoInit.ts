4b2c7ccf7ca31eefd60757a7a329c4a5cee88174
DaHyunOhe <peelsob4d@gmail.com>
2023-01-06 19:04:28 +0900

    test env

--- next/utils/client/kakaoInit.ts
+++ next/utils/client/kakaoInit.ts
@@ -1,7 +1,7 @@
 export const kakaoInit = () => {
   const kakao = (window as any).Kakao;
   if (!kakao.isInitialized()) {
-    kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+    kakao.init("c69513345bcfeb35598f9a14047bb434");
   }
 
   return kakao;
