8283d8dc941862d82130931d4ed019c6438a7cd3
DaHyunOhe <peelsob4d@gmail.com>
2023-01-09 10:41:07 +0900

    fix : change nodemailer account

--- next/utils/server/email.ts
+++ next/utils/server/email.ts
@@ -1,16 +1,16 @@
 import nodemailer from "nodemailer";
 
 const smtpTransport = nodemailer.createTransport({
-  service: "Naver",
-  host: "smtp.naver.com",
-  port: 587,
+  service: "gmail",
+  port: 465,
   auth: {
     user: process.env.MAIL_ID,
     pass: process.env.MAIL_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  secure: true,
+  // tls: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 export default smtpTransport;
